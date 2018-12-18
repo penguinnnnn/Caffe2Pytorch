0097d589bb784c36bb34b222a12199a8b62fe1c7
huangrenze <huangrenze@sensetime.com>
2018-12-18 11:50:50 +0800

    update

---
+++ README.md
@@ -0,0 +1,46 @@
+# Caffe2Pytorch
+
+### Introduction
+
+This is a tool for changing Caffe model to Pytorch model. I borrow the main framework from [xiaohang's CaffeNet](https://github.com/marvis/pytorch-caffe). I modify the structure and add more supports to them.
+
+Given a `.prototxt` and a `.caffemodel`, the conversion code generates a `.pth`. You can use the Pytorch model by the `.prototxt` and the `.pth`.
+
+There will be lots of layers in Caffe, it is impossible to know how all layers are constructed. Now the code supports these layers:
+
+```python
+'Data', 'AnnotatedData', 'Pooling', 'Eltwise', 'ReLU', 'PReLU', 'Permute', 'Flatten', 'Slice', 'Concat', 'Softmax', 'SoftmaxWithLoss', 'LRN', 'Dropout', 'Reshape', 'PriorBox', 'DetectionOutput'
+```
+
+### Dependency
+
+##### General requirement:
+
+`python2` or `python3` are both OK, depend on your `pycaffe` API.
+
+`pytorch` >= 0.4
+
+#####Special requirement:
+
+Only the conversion code requires `pycaffe`.
+
+### Usage
+
+##### Conversion
+
+```
+python caffe2pth_convertor.py \
+ --prototxt=YOUT_PROTOTXT_PATH \
+ --caffemodel=YOUT_CAFFEMODEL_PATH \
+ --pthmodel=OUTPUT_PTHMODEL_PATH
+```
+
+##### Use the model in Pytorch
+
+```python
+from caffe2pth.caffenet import *
+
+net = CaffeNet(YOUT_PROTOTXT_PATH)
+net.load_state_dict(torch.load(OUTPUT_PTHMODEL_PATH))
+```
+
---
+++ caffe2pth_convertor.py
@@ -0,0 +1,88 @@
+"""Convert a Caffe model to Pytorch
+
+Given a prototxt and a caffemodel, this code outputs a pth model.
+You can reconstruct the network by the prototxt and the pth model.
+
+Supported Caffe layers:
+    'Data', 'AnnotatedData', 'Pooling', 'Eltwise', 'ReLU', 'PReLU',
+    'Permute', 'Flatten', 'Slice', 'Concat', 'Softmax', 'SoftmaxWithLoss',
+    'LRN', 'Dropout', 'Reshape', 'PriorBox', 'DetectionOutput'
+    
+***Notice: This code requires pycaffe API***
+"""
+
+from caffe2pth.caffenet import *
+import argparse
+import caffe.proto.caffe_pb2 as caffe_pb2
+
+
+# Default input prototxt path
+_PROTOTXT_PATH = 'VGG19.prototxt'
+
+# Default input caffemodel path
+_CAFFEMODEL_PATH = 'VGG19.caffemodel'
+
+# Default output pytorch weights path
+_PTHMODEL_PATH = 'VGG19_CaffeNet.pth'
+
+
+def parse_caffemodel(caffemodel):
+    """Parse a caffemodel
+    
+    Inputs:
+      string of caffemodel path
+    Returns:
+      Parsed model
+    
+    This function should be in prototxt.py, but this function
+    requires caffe which requires python2. So move it here.
+    """
+    model = caffe_pb2.NetParameter()
+    print('Loading caffemodel: ' + caffemodel)
+    with open(caffemodel, 'rb') as fp:
+        model.ParseFromString(fp.read())
+
+    return model
+
+
+def main():
+    parser = argparse.ArgumentParser(
+                       description='Convert caffe to pytorch.')
+    parser.add_argument('--prototxt', type=str, default=_PROTOTXT_PATH,
+                       help='Caffe prototxt path.')
+    parser.add_argument('--caffemodel', type=str, default=_CAFFEMODEL_PATH,
+                       help='Caffe caffemodel path.')
+    parser.add_argument('--pthmodel', type=str, default=_PTHMODEL_PATH,
+                       help='Output pytorch model path.')
+    args = parser.parse_args()
+    
+    # Load network model
+    print('========================================')
+    print('Parsing Caffe model.')
+    print('----------------------------------------')
+    net = CaffeNet(args.prototxt)
+    
+    print('----------------------------------------')
+    print('Print converted pytorch model.')
+    print('----------------------------------------')
+    print(net)
+    
+    # Load network weights
+    print('----------------------------------------')
+    print('Loading weights.')
+    print('----------------------------------------')
+    net.load_weights(parse_caffemodel(args.caffemodel))
+    
+    # Save model structure as OrderedDict
+    print('----------------------------------------')
+    print('Saving pytorch weights.')
+    print('----------------------------------------')
+    torch.save(net.state_dict(), args.pthmodel)
+    
+    print('End.')
+    print('========================================')
+
+    
+if __name__ == '__main__':
+    main()
+
